2098838e0b7514a9f52da760fc9c3532bfb170f0
Nazarii Medvid <nmedvid41@gmail.com>
2026-03-17 21:17:03 +0200

    fix test to pass CI

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 from Flask import allowed_file
 
 def test_allowed_file_extension():
-    # Навмисно робимо помилку, щоб зламати CI
-    # Очікуємо True, але функція поверне False
-    assert allowed_file("document.pdf") == True
+    assert allowed_file("avatar.png") == True
